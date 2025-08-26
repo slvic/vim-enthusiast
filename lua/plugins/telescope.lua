6640346dc32ec5290a5adad8fb2cfe8ea4c89838
vyacheslaivanov <vyacheslaivanov@ozon.ru>
2025-08-26 22:18:44 +0300

    I hate neogit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,32 +18,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
   },
-  keys = {
-    { '<leader>sh', function() require('telescope.builtin').help_tags() end, desc = '[S]earch [H]elp' },
-    { '<leader>sk', function() require('telescope.builtin').keymaps() end, desc = '[S]earch [K]eymaps' },
-    { '<leader>sf', function() require('telescope.builtin').find_files() end, desc = '[S]earch [F]iles' },
-    { '<leader>ss', function() require('telescope.builtin').builtin() end, desc = '[S]earch [S]elect Telescope' },
-    { '<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord' },
-    { '<leader>sg', function() require('telescope.builtin').live_grep() end, desc = '[S]earch by [G]rep' },
-    { '<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = '[S]earch [D]iagnostics' },
-    { '<leader>sr', function() require('telescope.builtin').resume() end, desc = '[S]earch [R]esume' },
-    { '<leader>s.', function() require('telescope.builtin').oldfiles() end, desc = '[S]earch Recent Files' },
-    { '<leader><leader>', function() require('telescope.builtin').buffers() end, desc = '[ ] Find existing buffers' },
-    { '<leader>gg', function() require('telescope.builtin').git_status() end, desc = 'Find [G]iT Status' },
-    { '<leader>/', function()
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, desc = '[/] Fuzzily search in current buffer' },
-    { '<leader>s/', function()
-        require('telescope.builtin').live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, desc = '[S]earch [/] in Open Files' },
-    { '<leader>sn', function() require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' } end, desc = '[S]earch [N]eovim files' },
-  },
   config = function()
     -- Telescope is a fuzzy finder that comes with a lot of different things that
     -- it can fuzzy find! It's more than just a "file finder", it can search
@@ -78,6 +52,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
               ['<C-d>'] = require('telescope.actions').delete_buffer,
             },
           },
+          sort_mru = true,
+          sort_lastused = true,
+          initial_mode = 'normal',
+          theme = 'ivy',
         },
       },
       extensions = {

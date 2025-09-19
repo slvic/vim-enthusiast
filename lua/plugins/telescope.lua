f876feef57d6a8c7cf3a09703188c544a2d4b524
vyacheslaivanov <vyacheslaivanov@ozon.ru>
2025-09-19 16:33:23 +0300

    Lazy update, telescope fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,6 +25,30 @@ end
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   cmd = { 'Telescope' },
+  -- Define keymaps here so Lazy sets them immediately and loads Telescope on first use
+  keys = function()
+    local builtin = require 'telescope.builtin'
+
+    return {
+      { '<leader>sf', builtin.find_files, desc = '[S]earch [F]iles' },
+      { '<leader>ss', builtin.builtin, desc = '[S]earch [S]elect Telescope' },
+      { '<leader>sg', builtin.live_grep, desc = '[S]earch by [G]rep' },
+      { '<leader><leader>', builtin.buffers, desc = '[ ] Find existing buffers' },
+      { '<leader>gg', builtin.git_status, desc = 'Find [G]iT Status' },
+      { '<leader>gb', builtin.git_branches, desc = 'Find [G]iT [B]ranches' },
+      {
+        '<leader>sG',
+        function()
+          builtin.live_grep {
+            prompt_title = 'Go Module Files',
+            cwd = nil,
+            search_dirs = getGoModDirs(),
+          }
+        end,
+        desc = '[S]earch in [G]o mod',
+      },
+    }
+  end,
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -71,27 +95,5 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
-
-    -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>gg', builtin.git_status, { desc = 'Find [G]iT Status' })
-    vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Find [G]iT [B]ranches' })
-    vim.keymap.set('n', '<leader>sG', function()
-      builtin.live_grep {
-        prompt_title = 'Go Module Files',
-        cwd = nil, -- This will be set per entry
-        search_dirs = getGoModDirs(),
-      }
-    end, { desc = '[S]earch in [G]o mod' })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
   end,
 }

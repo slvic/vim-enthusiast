622cb113a4fc8f77f7feb60bb789f0856e64d882
vyacheslaivanov <vyacheslaivanov@ozon.ru>
2026-02-10 13:38:39 +0300

    Blink WIP

--- init.lua
+++ init.lua
@@ -13,9 +13,27 @@ require 'pack.which-key'
 require 'pack.treesitter'
 require 'pack.dap'
 
+vim.pack.add {
+  { src = 'https://github.com/rafamadriz/friendly-snippets' },
+}
 vim.pack.add({
   { src = 'https://github.com/saghen/blink.cmp' },
 }, { load = true })
 
+require('blink.cmp').setup {
+  keymap = { preset = 'default' },
+  appearance = {
+    nerd_font_variant = 'mono',
+  },
+  completion = {
+    documentation = { auto_show = false },
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+  fuzzy = {
+    implementation = 'prefer_rust_with_warning',
+  },
+}
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
